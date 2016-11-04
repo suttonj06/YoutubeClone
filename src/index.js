ecb1aab4e8ff88e0f21b1488f82cf02718560974
James Sutton <suttonj06@gmail.com>
2016-11-03 20:56:43 -0400

    Youtube Clone Reinitialization

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+import _ from 'lodash';
+const API_KEY = 'AIzaSyA77zjQwzxj5b7ePo1ap1twgqFVEMIu1aQ';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
+
+    this.videoSearch('dogs');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={ this.state.selectedVideo } />
+        <VideoList
+          videos={ this.state.videos }
+          onVideoSelect={
+            selectedVideo => this.setState({ selectedVideo })
+          }
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
